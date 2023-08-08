fe2cac62d7a3ae0a6d5f1722523c2e6d10e09b51
HylianMuffins <catholiczach@gmail.com>
2023-08-08 17:13:06 -0600

    board constructor now initializes all columns, rows and boxes

--- sudokusolver/Board.java
+++ sudokusolver/Board.java
@@ -10,10 +10,33 @@ package sudokusolver;
  * @since 2023-08-08
  */
 public class Board {
+    private Column[] columns = new Column[9];
+    private Row[] rows = new Row[9];
+    private InternalBox[][] boxes = new InternalBox[3][3];
     
     /**
      * Board constructor that creates a board instance.
      */
     public Board() {
+        // Inititalize all 9 columns
+        for (int columnNumber = 0; columnNumber < this.columns.length; 
+            columnNumber++) {
+                this.columns[columnNumber] = new Column(columnNumber);
+        }
+
+        // Initialize all 9 rows
+        for (int rowNumber = 0; rowNumber < this.rows.length; rowNumber++) {
+            this.rows[rowNumber] = new Row(rowNumber);
+        }
+
+        // Initialize all 9 boxes
+        for (int rowNumber = 0; rowNumber < boxes.length; rowNumber++) {
+            for (int columnNumber = 0; boxes[rowNumber].length < 3;
+                columnNumber++) {
+                    this.boxes[rowNumber][columnNumber] = 
+                        new InternalBox(rowNumber, columnNumber);
+            }
+        }
+
     }
 }
