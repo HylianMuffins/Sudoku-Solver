0d575b44078deef698cb600705ba0ed468472f0c
HylianMuffins <catholiczach@gmail.com>
2023-08-08 15:51:41 -0600

    Added board class

---
+++ sudokusolver/Board.java
@@ -0,0 +1,19 @@
+package sudokusolver;
+
+/**
+ * The Board class represents a full 9x9 sudoku board with 9 collumns, 9 rows,
+ * and 9 3x3 internal boxes. I will start in an unsolved state and will be
+ * solved following the sudoku puzzle rules.
+ * 
+ * @author Zachariah Blair
+ * @version 1.0
+ * @since 2023-08-08
+ */
+public class Board {
+    
+    /**
+     * Board constructor that creates a board.
+     */
+    public Board() {
+    }
+}
