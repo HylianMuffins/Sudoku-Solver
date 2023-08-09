73dd16d562b45ec21e28036425c08bc6ccb83b20
HylianMuffins <catholiczach@gmail.com>
2023-08-08 23:19:00 -0600

    added a UnitTesting class

---
+++ sudokusolver/UnitTesting.java
@@ -0,0 +1,22 @@
+package sudokusolver;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * The UnitTesting class tests the methods of the sudoku solver program using
+ * junit tests.
+ * 
+ * @author Zachariah Blair
+ * @version 1.0
+ * @since 2023-08-08
+ */
+public class UnitTesting {
+
+    public UnitTesting() {
+    }
+
+    @Test
+    public void testBoardConstructor() {
+    };
+}
